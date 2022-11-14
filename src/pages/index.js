5577bae532d54632623b568acb270c56f8795661
Matthew Mallory <40873286+mmallory56@users.noreply.github.com>
2022-11-14 15:16:48 -0600

    added cool css

--- src/pages/index.js
+++ src/pages/index.js
@@ -105,18 +105,51 @@ display: grid;
 grid-template-columns: 10% 10% 10% 10%;
 align-items: center;
 justify-content: space-evenly;
-font-size: 70px;
-color: white;
+font-size: 60px;
+background-color: #18191f00;
+
+& svg{
+  
+background-color: #8484843f;
+color: #ffffffff;
+
+
+border-radius: 29px; padding: 11px 19px;
+margin: 0 40px;
+animation: animate 9s linear infinite; 
+animation-direction: alternate-reverse;
+transition: filter 2s ease-in-out;
+
+;
+@keyframes animate {
+  0%   {filter: drop-shadow( 0 0 10px #121f83) drop-shadow( 0 0 20px #01148e)
+drop-shadow( 0 0 30px #001c82) drop-shadow( 0 0 35px #0062ff) }
+  25%  {filter: drop-shadow( 0 0 10px #ff0000) drop-shadow( 0 0 20px #8e0101)
+drop-shadow( 0 0 30px #000082) drop-shadow( 0 0 35px #ff0000)}
+  50%  {filter: drop-shadow( 0 0 10px #ff0000) drop-shadow( 0 0 20px #01688e)
+drop-shadow( 0 0 30px #004a3e) drop-shadow( 0 0 35px #004d50) }
+  75%  {filter: drop-shadow( 0 0 10px #005a4e) drop-shadow( 0 0 20px #004461)
+drop-shadow( 0 0 30px #006482) drop-shadow( 0 0 35px #ff00e1) }
+  100% {filter: drop-shadow( 0 0 10px #f700ff) drop-shadow( 0 0 20px #87018e)
+drop-shadow( 0 0 30px #820000) drop-shadow( 0 0 350px #ff0000) }
+}
+& svg :nth-of-type(2n + 1){
+animation-delay: 3s;
+}
+
+}
 
 & svg:hover{
   font-size: 100px;
-  color: lightcyan;
-  text-shadow: 10px 10px 10px gray;
+ 
+  
   transition: all .5s ease-in;
   &:div:hover{
     opacity:1;
   }
-}
+
+} 
+
 @media (max-width: 400px) {
   font-size: 40px;
   margin:5px;
